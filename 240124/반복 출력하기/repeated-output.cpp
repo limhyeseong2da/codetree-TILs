8c81ca6588142b2fbcadea0dc69c336c49f33a70
limhyeseong2da <144296507+limhyeseong2da@users.noreply.github.com>
2024-01-24 16:56:19 +0900

    Title: 반복 출력하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240124/반복 출력하기/repeated-output.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+void printNlines(int n){
+
+    for(int i=0;i<n;i++){
+        cout<<"12345^&*()_";
+        cout<<"\n";
+    }
+
+}
+int main() {
+    int N;
+    cin>>N;
+    printNlines(N);
+    return 0;
+}
