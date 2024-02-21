db117344e793a57bccb48aa41dd2bf73c39ee3ad
limhyeseong2da <144296507+limhyeseong2da@users.noreply.github.com>
2024-02-21 21:03:15 +0900

    Title: 오름 내림차순 정렬, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240221/오름 내림차순 정렬/inc-dec-sorting.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <algorithm>
+#include <functional>
+
+using namespace std;
+
+
+int main() {
+	int n;
+	cin >> n;
+
+	int* arr = new int[n];
+
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+
+	sort(arr, arr + n);
+
+	for (int i = 0;i < n;i++) {
+		cout << arr[i] << " ";
+	}
+	cout << "\n";
+	sort(arr, arr + n, greater<int>());
+
+	for (int i = 0;i < n;i++) {
+		cout << arr[i] << " ";
+	}
+
+}
