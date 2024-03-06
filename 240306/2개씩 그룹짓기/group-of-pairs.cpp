577383d87fe72eafa2cc137749d999d0724ef190
limhyeseong2da <144296507+limhyeseong2da@users.noreply.github.com>
2024-03-06 16:51:37 +0900

    Title: 2개씩 그룹짓기, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 240306/2개씩 그룹짓기/group-of-pairs.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+    int n;
+    cin>>n;
+
+    for(int i=0;i<2n;i++){
+        cin>>vec[i];
+    }
+
+    sort(vec.begin(),vec.end());
+
+    cout<<vec[2n-1]+vec[n-1];
+
+
+
+    return 0;
+}
