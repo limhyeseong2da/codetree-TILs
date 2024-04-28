f12a97b25d0f3782a14f980b7698b961e6999f66
임혜성 <144296507+limhyeseong2da@users.noreply.github.com>
2024-04-28 22:28:32 +0900

    Title: DateTime to DateTime, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240428/DateTime to DateTime/datetime-to-datetime.cpp
+++ 240428/DateTime to DateTime/datetime-to-datetime.cpp
@@ -1,12 +1,20 @@
 #include <iostream>
 using namespace std;
+
 int main() {
-    int a,b,c;
-    cin>>a>>b>>c;
-    if(a>10){
-        cout<<(24*(a-11)*60+b*60+c)-(11*60+11);}
-    else{
-         cout<<11*60+11-(a*60+b*60+c);
-    }
+    int a, b, c;
+    cin >> a >> b >> c;  // 사용자 입력: 11월 a일 b시 c분
+    
+    // 11월 11일 11시 11분과의 차이를 계산
+    int days_diff = a - 11;   // 일수 차이
+    int minutes_diff = days_diff * 24 * 60;  // 일수 차이를 분으로 환산
+    
+    // 시간 차이와 분 차이 추가 계산
+    minutes_diff += (b - 11) * 60;
+    minutes_diff += (c - 11);
+    
+    // 결과 출력
+    cout << minutes_diff << endl;
+    
     return 0;
 }
