b38b7742a8d02d719c0cf328bf6e3289cdb0e27e
임혜성 <144296507+limhyeseong2da@users.noreply.github.com>
2024-05-01 16:10:44 +0900

    Title: 연속되는 수 3, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240501/연속되는 수 3/continuous-number3.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+    int n;
+    cin>>n;
+    vector<int> vec(n,0);
+    for(int i=0;i<n;i++){
+        cin>>vec[i];
+    }
+    int cnt=1;
+    int max=0;
+    for(int i=1;i<n;i++){
+        if(vec[i]*vec[i-1]>0){
+            cnt++;
+        }
+        else{
+            if(max<cnt){
+                max=cnt;
+            }
+            cnt=1;
+        }
+    }
+
+    if(max<cnt){
+        max=cnt;
+    }
+
+    cout<<max;
+
+    return 0;
+}
