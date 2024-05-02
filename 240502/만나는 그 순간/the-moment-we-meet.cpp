6a4d48b517a0bc2c3ba3662f31de4e1f1bf2a824
임혜성 <144296507+limhyeseong2da@users.noreply.github.com>
2024-05-02 14:50:33 +0900

    Title: 만나는 그 순간, Time: 13ms, Memory: 5MB, Status: Passed - CodeTree

--- 240502/만나는 그 순간/the-moment-we-meet.cpp
+++ 240502/만나는 그 순간/the-moment-we-meet.cpp
@@ -1,22 +1,26 @@
 #include <iostream>
+#include <vector>
+#include <algorithm>
 using namespace std;
 
 int main() {
     int n,m;
     cin>>n>>m;
-    int arr[1000000]={0};
-    int arr1[1000000]={0};
+    vector<int> arr;
+    vector<int> arr1;
     int start_num=1;
     int start_numb=1;
+    arr.push_back(0);
+    arr1.push_back(0);
 
     for(int i=0;i<n;i++){
         int t;
         char ch;
         cin>>ch>>t;
-      
+
         if(ch=='R'){
             for(int j=1;j<t+1;j++){
-                arr[start_num]=arr[start_num-1]+1;
+                arr.push_back(arr.back()+1);
                 start_num++;
             }
            
@@ -24,7 +28,7 @@ int main() {
 
         if(ch=='L'){
             for(int j=1;j<t+1;j++){
-                arr[start_num]=arr[start_num-1]-1;
+                arr.push_back(arr.back()-1);
                 start_num++;
             }
         }
@@ -38,7 +42,7 @@ int main() {
 
         if(ch=='R'){
             for(int j=1;j<t+1;j++){
-                arr1[start_numb]=arr1[start_numb-1]+1;
+                arr1.push_back(arr1.back()+1);
                 start_numb++;
             }
            
@@ -46,14 +50,15 @@ int main() {
 
         if(ch=='L'){
             for(int j=1;j<t+1;j++){
-                arr1[start_numb]=arr1[start_numb-1]-1;
+                arr1.push_back(arr1.back()-1);
                 start_numb++;
             }
         }
     }
     int ans=-1;
+    int check=min(arr.size(),arr1.size());
 
-    for(int i=1;i<1000000;i++){
+    for(int i=1;i<check;i++){
         if(arr[i]==arr1[i]){
             ans=i;
             break;
