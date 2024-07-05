a326908fbf405bd573506e4e67b50b3aafc38a45
임혜성 <144296507+limhyeseong2da@users.noreply.github.com>
2024-07-06 04:06:40 +0900

    Title: 숫자의 증감, Time: 99ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240706/숫자의 증감/increasing-and-decreasing-numbers.py
@@ -0,0 +1,13 @@
+inp=input().split()
+
+a=inp[0]
+b=int(inp[1])
+
+if a=="A":
+    i=1
+    for i in range (i,b+1):
+        print(i,end=' ')
+else:
+    t=0
+    for t in range(b,t,-1):
+        print(t,end=' ')
