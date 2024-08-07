51d8254af85a3818763fbb547c4cceec85580466
임혜성 <144296507+limhyeseong2da@users.noreply.github.com>
2024-08-07 10:46:26 +0900

    Title: ab 사이에 없는 c, Time: 178ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240807/ab 사이에 없는 c/c-between-a-and-b-2.py
@@ -0,0 +1,13 @@
+inp=input().split()
+a,b,c=int(inp[0]),int(inp[1]),int(inp[2])
+chk=False
+
+for i in range (a,b+1):
+    if i%c==0:
+        chk=True
+        break
+
+if chk==True:
+    print("NO")
+else:
+    print("YES")
